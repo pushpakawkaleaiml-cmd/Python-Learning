72395dc8c48e4d3cd28a5fe29bf35f154b23d9a7
Pushpak <pushpakawkaleaiml@gmail.com>
2026-09-17 23:42:10 +0530

    Add RPG character builder

---
+++ rpg_character.py
@@ -0,0 +1,34 @@
+full_dot = '●'
+empty_dot = '○'
+def create_character(character_name,strength,intelligence,charisma):
+    if not(isinstance(character_name, str)):
+        return 'The character name should be a string'     
+    elif (len(character_name)==0):
+        return 'The character should have a name'
+    elif (len(character_name)>10):
+        return 'The character name is too long'
+    elif (' 'in character_name):
+        return 'The character name should not contain spaces'
+    elif (not(isinstance(strength,int)) or not(isinstance(intelligence,int)) or not(isinstance(charisma,int))):
+        return 'All stats should be integers'
+    elif (strength<1 or intelligence<1 or charisma<1):
+        return 'All stats should be no less than 1'
+    elif (strength >4 or intelligence>4 or charisma>4):
+        return 'All stats should be no more than 4'
+    elif (strength+intelligence+charisma!=7):
+        return 'The character should start with 7 points'
+
+    return (
+        character_name + '\n'
+        + 'STR ' + full_dot * strength + empty_dot * (10 - strength) + '\n'
+        + 'INT ' + full_dot * intelligence + empty_dot * (10 - intelligence) + '\n'
+        + 'CHA ' + full_dot * charisma + empty_dot * (10 - charisma)
+    )
+character_name = input('Enter character name: ')
+
+strength = int(input('Enter strength (1-4): '))
+intelligence = int(input('Enter intelligence (1-4): '))
+charisma = int(input('Enter charisma (1-4): '))
+
+
+print(create_character(character_name, strength, intelligence, charisma))
